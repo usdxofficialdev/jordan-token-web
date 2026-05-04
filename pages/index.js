4d41e848d9c24ef5b6b2e8bef4c22c9fb9dc208f
devjordan330 <devjordan330@gmail.com>
2026-05-04 09:40:45 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: 'black', color: 'white', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'sans-serif' }}>
+      <h1>MetaWorld is Coming Soon...</h1>
+    </div>
+  )
+}
